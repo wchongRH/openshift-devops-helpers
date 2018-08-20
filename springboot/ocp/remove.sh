14a3e41ea04cb545bf04f95e3d0f29956f3218c9
wchongRH <36657400+wchongRH@users.noreply.github.com>
2018-08-20 14:51:26 +0800

    Delete PV after project deletion completes, not during.
    Include check to see if PV is still bound to project; if not, wait till project deletion completes before deleting PV.

--- springboot/ocp/remove.sh
+++ springboot/ocp/remove.sh
@@ -67,7 +67,33 @@ if [ -d ${PV_DIR}/${STAGE} ]; then
                 pv_name=`echo $pv | cut -d. -f1`
 
                 set +x; echo -e "\n### Deleting pv $pv_name for $PROJ ##########\n"; set -x
-                $OC_DEPLOY_ADMIN_CLI delete pv $pv_name-${APP}-${STAGE}-${RELTAG}
+                ################# Check if PV is still bound #######################################
+                # A persistent volume can only be deleted if it is not still bound to a project.   #
+                # As a race may occur between the completion of the deletion of a project (the oc  #
+                # command "$OC_DEPLOY_CLI delete project $PROJ" above) and the command to delete   #
+                # the persistent volume below (the oc command "$OC_DEPLOY_ADMIN_CLI delete pv...") #
+                # the check below is made to wait till the persistent volume is not bound before   #
+                # the command to delete the persistent volume is issued.                           #
+                # The check will run for 5 times, waiting 30 seconds each time.                    #
+                RETRY=5
+                while [ $($OC_DEPLOY_ADMIN_CLI get pv $pv_name-${APP}-${STAGE}-${RELTAG_URL} -o jsonpath="{.status.phase}") = "Bound" ]
+                do
+                  echo "Can't delete PV $pv_name-${APP}-${STAGE}-${RELTAG} yet. Still bound. Sleep for 30s."
+                  sleep 30
+                  let "RETRY = RETRY - 1"
+                  if [ 0 -eq ${RETRY} ]
+                  then
+                    echo "Retried ${RETRY} times, PV still bound"
+                    break
+                  fi
+                done
+                ############### End of check for bound PV #########################################
+                if [ $($OC_DEPLOY_ADMIN_CLI get pv $pv_name-${APP}-${STAGE}-${RELTAG_URL} -o jsonpath="{.status.phase}") = "Released" ]; then
+                  $OC_DEPLOY_ADMIN_CLI delete pv $pv_name-${APP}-${STAGE}-${RELTAG_URL}
+                  echo "$pv_name-${APP}-${STAGE}-${RELTAG_URL} deletion succeeded."
+                else
+                  echo "$pv_name-${APP}-${STAGE}-${RELTAG_URL} deletion failed."
+                fi
         done
 fi
 
